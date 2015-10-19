796d65116bb4939a20b34bdcb0615412deb1774a
Jesper "JAHugawa" Hjorth <jesper.hjorth@hiit.fi>
2015-10-19 17:45:58 +0300

    Integrated startLogging to master server

--- fbalog/KuplassaServer.py
+++ fbalog/KuplassaServer.py
@@ -1,32 +1,76 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+
+# JAHugawa's ToDo corner:
+# DOJO: check if the logging path works >.<
+# DOJO: check shell=True vulnerabilities!
 # DOJO: python 2 (because of server configs >.<) so does not work with python3
 
 from cgi import escape
 #from flup.server.fcgi import WSGIServer
+import json
 import os
+import shlex
+import subprocess
 import sys
+import urllib.request
+import urlparse # for parse_qs
 from wsgiref.simple_server import make_server
+
+from AppConfiguration import *
+from DbConnection import *
+from FBDataFetcher import *
+import TextToHtml as T2H
+
 
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
+    
+    loginSuccessful = True # change to False when something goes horribly wrong
+    error = ""
 
-    yield '<html><body><p>Kuplaa rakennetaan...</p></body></html>'
-    # <img src="https://utmemes.com/images/under_construction_cat_soap.jpg></img>'
-    #yield '<table>'
-    for k, v in sorted(environ.items()):
-        break
-        if type(k) == str:
-            kShow = escape(k)
+    # handle errors / fetch access token
+    getParams = urlparse.parse_qs(environ["QUERY_STRING"])
+    if "error_reason" and "error" and "error_description" in getParams: #user denied request
+        loginSuccessful = False
+        error = getParams["error_description"].value
+    elif "code" in getParams:
+        code = getParams["code"].value
+        # proceed by sending access token request
+        response = None
+        try:
+            response = urllib.request.urlopen("https://graph.facebook.com/v2.4/oauth/access_token?client_id="+APP_ID+"&redirect_uri="+REDIRECT_URI+"&response_type=code&scope=user_likes,user_posts,user_status&client_secret="+APP_SECRET+"&code="+code)
+        except:
+            loginSuccessful = False
+            error = "Unknown error when starting logging. Please try again."
         else:
-            kShow = escape("NON-STRING KEY")
-        if type(v) == str:
-            vShow = escape(v)
-        else:
-            vShow = escape("NON-STRING VALUE")
-            print type(v)
-        yield '<tr><th>{0}</th><td>{1}</td></tr>'.format(kShow,vShow)
-    #yield '</table>'
+            if response.getcode() not in [200]:
+                loginSuccessful = False
+                error = "fetching long-term token failed with code "+response.getcode()
+            else:
+                d = json.loads(response.read())
+                if set(["access_token","token_type","expires_in"]).issubset(set(d.keys())):
+                    accessToken = d["access_token"]
+                    # push access token to database
+                    fb = FBDataFetcher()
+                    userId = fb.fetchUserId(accessToken)
+                    db = DbConnection()
+                    db.setAccessToken(userId,accessToken)
+                    # start remote process to mine the data, then proceed to tell the user that logging has successfully started
+                    p = subprocess.Popen(args="python3 mineFB.py "+shlex.quote(userId), shell=True)
+                else:
+                    loginSuccessful = False
+                    error = "response was missing data (access_token, token_type or expires_in)"
+
+    else:
+        loginSuccessful = False
+        error = "missing authentication info (code). Try again"
+
+    # end this process by returning success message
+    if loginSuccessful:
+        yield """<html><body><h1>Login successful! The code is crunching...</h1></body></html>"""
+    else:
+        yield """<html><body><h1>Login failed! Reason: """+T2H.convert(error)+"""</h1></body></html>"""
 
 port = 9090
 kuplassaServer = make_server('localhost', port, app)

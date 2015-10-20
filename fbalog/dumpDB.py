871b002c61b38d97654bcdb0f86e42dc30b9ae7e
Jesper "JAHugawa" Hjorth <jesper.hjorth@hiit.fi>
2015-10-20 13:22:35 +0300

    Added script to dump sqlite data to json

---
+++ fbalog/dumpDB.py
@@ -0,0 +1,28 @@
+import json
+import sys
+import time
+
+from AppConfiguration import *
+from DbConnection import *
+from FBDataFetcher import *
+
+if __name__ == "__main__":
+    db = DbConnection()
+    fb = FBDataFetcher()
+    
+    userId = []
+    if len(sys.argv) >= 2:
+        userId += sys.argv[1:]
+    else:
+        # if no user ids are supplied, fetch all users
+        userId += db.getAllUserIds()
+    
+    # dump posts to json
+    posts = []
+    for uid in userId:
+        posts += db.getUserActivity(uid)
+    postsJson = json.dumps(posts)
+    currentTime = time.strftime("%Y_%m_%d_%H%M%S", time.gmtime())
+    with open("posts_%s.json" % (currentTime,),"w") as f:
+        f.write(postsJson)
+

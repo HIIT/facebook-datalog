852e38947b554bfa33241e8c4e2c6617fba88a69
Jesper "JAHugawa" Hjorth <jesper.hjorth@hiit.fi>
2016-04-25 14:09:52 +0300

    fbpetj: Made script to autofetch datasets in 1-year segments

---
+++ fbpetj/run_per_year.sh
@@ -0,0 +1,17 @@
+authPath="token.auth"
+startYear=2000
+finishYear=2017
+
+currentYear=$startYear
+
+# quick test!
+python3 fetchEvents.py -a $authPath -s 2016-04-20 -u 2016-04-24
+
+while [ currentYear -le finishYear ]
+do
+    s=$currentYear-01-01
+    u=`expr $currentYear + 1`-01-01
+    python3 fetchEvents.py -a $authPath -s $s -u $u
+
+    currentYear = `expr $currentYear + 1`
+done

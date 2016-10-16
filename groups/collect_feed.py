a4446cbac5afae39fd4023be91abe50261b159a2
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2016-10-16 23:34:27 +0300

    Minor refactor to take into account different types of objects we might collect

--- groups/collect_feed.py
+++ groups/collect_feed.py
@@ -8,6 +8,26 @@ app_secret = keys['app-secret']
 
 graph = facebook.GraphAPI(access_token= app_id + '|' + app_secret, version='2.7')
 
+def collect_feed( graph, id ):
+
+    ## collect posts
+    ## TODO: pagination to be added
+    data = {}
+
+    posts = graph.get_connections( id = id , connection_name='feed' )
+    posts = posts['data']
+
+    for post in posts:
+
+        data[ post['id'] ] =  graph.get_object( post['id'] ,
+            fields ='id,from,created_time,application,description,message,to,updated_time'
+        ) ## to be compleated
+
+        data[ post['id'] ][ '__comments' ] = graph.get_connections( id = post['id'], connection_name='comments' )['data']
+        data[ post['id'] ][ '_likes' ] = graph.get_connections( id = post['id'], connection_name='likes' )['data']
+
+    return data.values()
+
 for line in open('wave0.txt'):
 
     try:
@@ -17,24 +37,16 @@ for line in open('wave0.txt'):
         line = line.split('.com/')[1]
         line = line.replace('/', '')
 
-        group = graph.get_object(id= line)
-
-        ## collect posts
-        ## TODO: pagination to be added
+        fbobject = graph.get_object(id= line, fields='id,name', metadata = '1')
+        fbid = fbobject['id']
+        fbtype = fbobject['metadata']['type']
 
         data = {}
 
-        posts = graph.get_connections( id = group['id'], connection_name='feed' )
-        posts = posts['data']
+        if fbtype in ['page', 'group', 'event', 'user']:
+            data['feed'] = collect_feed( graph, fbid )
 
-        for post in posts:
-
-            data[ post['id'] ] =  graph.get_object( post['id'] ,
-                fields ='id,from,created_time,application,description,message,to,updated_time'
-            ) ## to be compleated
-
-            data[ post['id'] ][ '__comments' ] = graph.get_connections( id = post['id'], connection_name='comments' )['data']
-            data[ post['id'] ][ '_likes' ] = graph.get_connections( id = post['id'], connection_name='likes' )['data']
+        ## TODO: add group and page metadata collection
 
         json.dump( data, open( 'data_' + line + '.json', 'w' ) )
 

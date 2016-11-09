a147a1f82efb3f840ea85b0f740846ebd79332ad
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2016-11-09 15:29:45 +0200

    Take all methods into use

--- groups/collect_feed.py
+++ groups/collect_feed.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.insert( 0, '/Users/mnelimar/code/facebook-sdk/' ) ## temporary hack to use newer version of SDK
+
 import facebook
 import requests
 import json
@@ -7,6 +10,12 @@ app_id = keys['app-id']
 app_secret = keys['app-secret']
 
 graph = facebook.GraphAPI(access_token= app_id + '|' + app_secret, version='2.7')
+
+def _unfold( generator ): ## TODO: this shoud not be needed really
+    r = []
+    for i in generator:
+        r.append( i )
+    return r
 
 def collect_feed( graph, id ):
 
@@ -23,8 +32,10 @@ def collect_feed( graph, id ):
             fields ='id,from,created_time,application,description,message,to,updated_time'
         ) ## to be compleated
 
-        data[ post['id'] ][ '__comments' ] = graph.get_connections( id = post['id'], connection_name='comments' )['data']
-        data[ post['id'] ][ '_likes' ] = graph.get_connections( id = post['id'], connection_name='likes' )['data']
+
+
+        data[ post['id'] ][ '__comments' ] = _unfold( graph.get_all_connections( id = post['id'], connection_name='comments' ) )
+        data[ post['id'] ][ '_likes' ] = _unfold( graph.get_all_connections( id = post['id'], connection_name='likes' ) )
 
     return data.values()
 
@@ -53,7 +64,7 @@ for line in open('wave0.txt'):
 
         ## TODO: add group and page metadata collection
 
-        json.dump( data, open( 'data_' + fbobject['name'].replace(' ', '_').lower() + '.json', 'w' ) )
+        json.dump( data, open( 'data/data_' + fbobject['name'].replace(' ', '_').lower() + '.json', 'w' ) )
 
 
     except facebook.GraphAPIError:
